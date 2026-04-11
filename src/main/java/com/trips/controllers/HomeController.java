96e9ed986361e59896e001d94bf080ffcfc496d2
cesarsanchezcore <wcesarw@gmail.com>
2026-04-11 10:38:38 -0600

    Se agrego controlador y un html

---
+++ src/main/java/com/trips/controllers/HomeController.java
@@ -0,0 +1,19 @@
+package com.trips.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String mostrarHome (Model model) {
+		
+		model.addAttribute("mensaje", "Hola mundo");
+
+		return "home";
+	}
+	
+}

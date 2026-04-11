d481a5e54cc562c6acb6f04706a023564e300c48
cesarsanchezcore <wcesarw@gmail.com>
2026-04-11 12:12:48 -0600

    primer commit de agregando funcionalidades

--- src/main/java/com/trips/controllers/HomeController.java
+++ src/main/java/com/trips/controllers/HomeController.java
@@ -1,5 +1,8 @@
 package com.trips.controllers;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,13 +10,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-
+	
 	@GetMapping("/")
 	public String mostrarHome (Model model) {
 		
 		model.addAttribute("mensaje", "Hola mundo");
 
 		return "home";
+	}
+	
+	@GetMapping("/listado")
+	public String mostrarListado(Model model) {
+		
+		List<String> lista = new LinkedList<String>();
+		lista.add("En la Montaña");
+		lista.add("En la Ciudad");
+		lista.add("En los pueblos");
+		lista.add("En las playas");
+		
+		model.addAttribute("listadoTrips", lista);
+		
+		return "listado";
 	}
 	
 }
